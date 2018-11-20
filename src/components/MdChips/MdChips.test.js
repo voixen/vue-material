57053a73b7de66db0f0ac54eaada2dfaacd97fe3
Andre Bunse <andre.bunse@gmail.com>
2018-11-20 22:03:32 +0100

    remove click handler and use default behaviour, fixes tests

--- src/components/MdChips/MdChips.test.js
+++ src/components/MdChips/MdChips.test.js
@@ -5,12 +5,12 @@ test('should render the chips', async () => {
   const template = '<md-chips></md-chips>'
   const wrapper = await mountTemplate(MdChips, template)
 
-  expect(wrapper.hasClass('md-chips')).toBe(true)
+  expect(wrapper.find('.md-field')[0].hasClass('md-chips')).toBe(true)
 })
 
 test('should render the theme class', async () => {
   const template = '<md-chips md-theme="alt"></md-chips>'
   const wrapper = await mountTemplate(MdChips, template)
 
-  expect(wrapper.hasClass('md-theme-alt')).toBe(true)
+  expect(wrapper.find('.md-field')[0].hasClass('md-theme-alt')).toBe(true)
 })
